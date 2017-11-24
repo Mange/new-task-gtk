db7c8c24f3dd345a111f1e6a043b166530e09f55
Magnus Bergmark <magnus.bergmark@gmail.com>
2017-11-24 11:30:02 +0100

    Add support for styling window with CSS

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate gtk;
+extern crate xdg;
 
 use std::rc::Rc;
 use gtk::prelude::*;
-use gtk::{Entry, Window, WindowType, TextBuffer, TextView, Orientation, Builder, Revealer};
+use gtk::{Entry, Window, WindowType, TextBuffer, TextView, Orientation, Builder, Revealer,
+          CssProvider, StyleContext};
+use xdg::BaseDirectories;
 
 struct App {
     window: Window,
@@ -42,6 +45,35 @@ impl App {
     }
 }
 
+fn apply_custom_stylesheets(app: &App) -> bool {
+    let css_provider = CssProvider::new();
+
+    css_provider.connect_parsing_error(|_, _section, error| {
+        eprintln!("Could not load stylesheet: {}", error);
+    });
+
+    let base_dir =
+        BaseDirectories::with_prefix("new-task-gtk").expect("Could not access your HOME directory");
+
+    if let Some(style_file) = base_dir.find_config_file("style.css") {
+        if let Some(utf8_path) = style_file.to_str() {
+            if css_provider.load_from_path(utf8_path).is_err() {
+                return false;
+            }
+        } else {
+            eprintln!(
+                "Could not load stylesheet; file path not valid UTF-8: {}",
+                style_file.to_string_lossy(),
+            );
+        }
+    }
+
+    let screen = app.window.get_screen().unwrap();
+    StyleContext::add_provider_for_screen(&screen, &css_provider, 100);
+
+    true
+}
+
 fn main() {
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
@@ -49,6 +81,10 @@ fn main() {
     }
 
     let app = Rc::new(App::build(include_str!("window.glade")));
+
+    if !apply_custom_stylesheets(&app) {
+        std::process::exit(1);
+    }
 
     app.window.show_all();
 
